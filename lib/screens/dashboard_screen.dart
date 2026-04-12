457bb97bb75f8ddee3c549cdcdb85db0cb1c7924
chetpro_bot <id@gmail.com>
2026-04-12 10:16:12 +0900

    v1.3.2: bump version

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+﻿import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../services/auth_service.dart';
 import 'package:intl/intl.dart';
@@ -94,7 +94,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('나의 골프 기록 (v1.3.1)'),
+        title: const Text('나의 골프 기록 (v1.3.2)'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         leading: IconButton(
           icon: const Icon(Icons.logout, size: 20),
@@ -303,7 +303,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
         height: 30,
         alignment: Alignment.center,
         child: Text(
-          'App Version v1.3.1',
+          'App Version v1.3.2',
           style: TextStyle(fontSize: 10, color: Colors.grey.withOpacity(0.5)),
         ),
       ),
